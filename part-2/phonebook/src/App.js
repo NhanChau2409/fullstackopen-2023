166345e975ea3d7d683c46d0ca85d2e05e907f27
Nhan Chau <nhan.chau@tuni.fi>
2023-04-21 04:08:33 +0300

    phonebook step 5

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import { Header, Form, Persons, Filter } from './Components'
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456' },
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [filter, setFilter] = useState('')
 
+	useEffect(() => {
+		axios.get('http://localhost:3001/persons').then((response) => {
+			setPersons(response.data)
+		})
+	})
 	const submitHandler = (event) => {
 		event.preventDefault()
 
